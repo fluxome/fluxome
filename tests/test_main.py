d05a27155e01f83bb8e0231895eb5813bc341881
Cameron Smith <cameron.ray.smith@gmail.com>
2023-03-15 23:51:12 -0400

    update tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 import pytest
 from click.testing import CliRunner
 
-from igtme import __main__
+from igtme.cli import igtme
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(igtme)
     assert result.exit_code == 0
